bdcf20ba9f3ac410f1498c4f13cc84e2b11bfd66
KrishCM <44927837+KrishCM25@users.noreply.github.com>
2024-10-15 14:39:35 -0500

    HTTP HTTPS

--- src/app.js
+++ src/app.js
@@ -1,27 +1,28 @@
 import express from 'express';
 import path from 'path';
+
 const app = express();
 
 // Middleware para servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, '/public')));
 
-// Middleware para redirigir HTTP a HTTPS
+// Middleware para redirigir HTTP a HTTPS (solo si no está usando HTTPS)
 app.use((req, res, next) => {
-  if (!req.secure) {
-    // Redirigir a HTTPS
-    return res.redirect(`https://${req.headers.host}${req.url}`);
-  }
-  next();
+    if (!req.secure) {
+        // Redirigir a HTTPS
+        return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
 });
 
 // Ruta para manejar GET /
 app.get('/', (req, res) => {
-  res.send('¡Bienvenido a CampinHouse!');
+    res.send('¡Bienvenido a CampinHouse!');
 });
 
 // Ruta para API
 app.get('/api', (req, res) => {
-  res.send('API está funcionando');
+    res.send('¡API está funcionando!');
 });
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import fs from 'fs';
 import https from 'https';
+import http from 'http';
 import { Server as WebSocketServer } from 'socket.io';
-import express from 'express';
 import Sockets from './sockets';
 import { connectDB } from './db';
 import { PORT } from './config';
-import app from "./app";
-
+import app from './app';
 
 // Leer los archivos del certificado SSL
 const privateKey = fs.readFileSync("/etc/letsencrypt/live/campinhouse.com/privkey.pem", "utf8");
@@ -17,13 +16,25 @@ const credentials = { key: privateKey, cert: certificate, ca: ca };
 
 connectDB();
 
+// Crear servidor HTTPS
 const httpsServer = https.createServer(credentials, app);
 const io = new WebSocketServer(httpsServer);
 
 // Iniciar servidor HTTPS
 httpsServer.listen(PORT, () => {
-  console.log(`Servidor corriendo en HTTPS en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en HTTPS en el puerto ${PORT}`);
 });
 
 // Configurar los sockets
 Sockets(io);
+
+// Crear servidor HTTP para redirigir a HTTPS
+const httpServer = http.createServer((req, res) => {
+    res.writeHead(301, { "Location": `https://${req.headers.host}${req.url}` });
+    res.end();
+});
+
+// Escucha en el puerto 80 para HTTP
+httpServer.listen(80, () => {
+    console.log('Servidor HTTP escuchando en el puerto 80 y redirigiendo a HTTPS');
+});
