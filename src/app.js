047bbce986fe437d189c3f217e98010f54c91084
KrishCM <44927837+KrishCM25@users.noreply.github.com>
2024-10-15 20:29:53 -0500

    AXIOS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 import express from 'express';
 import path from 'path';
+import axios from 'axios';
 
 const app = express();
+
+
+
+// Datos estáticos (puedes cambiar esto para recibir el DNI desde el cliente)
+const token = 'apis-token-11030.SCSv4kKYWlHpNtJT2xmm5h0Wd4NEHhOw';
+
+// Ruta para consultar el DNI
+app.get('/consulta-dni/:dni', async (req, res) => {
+    const dni = req.params.dni;
+    
+    try {
+        const response = await axios.get(`https://api.apis.net.pe/v2/reniec/dni?numero=${dni}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Referer': 'https://apis.net.pe/consulta-dni-api'
+            }
+        });
+        
+        // Enviar la respuesta al cliente
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error al consultar el DNI:', error);
+        res.status(500).json({ message: 'Error al consultar el DNI' });
+    }
+});
+
+
 
 // Middleware para permitir solo campinhouse.com en iframes
 app.use((req, res, next) => {
