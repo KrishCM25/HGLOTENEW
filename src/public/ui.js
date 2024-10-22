1a21bd9dc2e7e47143ff4136f836dcaa4fbb6c31
KrishCM <44927837+KrishCM25@users.noreply.github.com>
2024-10-21 21:05:26 -0500

    STLES F 9830 987sadsdss

--- src/public/ui.js
+++ src/public/ui.js
@@ -32,16 +32,16 @@ const noteUI = (note) => {
     imageRegalo = '/assets/images/bici-hg-98.webp';
   }
     // Convertir el objeto dniInfo a string usando JSON.stringify
+    // <div class="regalo-elem-reserva-lt data-elem-reserva-lt">${note.regalo} </div>
   div.classList.add('elem-reserva-data-lt');
   div.innerHTML = `
     <div class="container-elem-reserva-data-lt">
       <div class="container-data-elem-reserva-lt">
-        <div class="image-regalo-elem-reserva-lt"><img src=""></div>
+        <div class="image-regalo-elem-reserva-lt"><img src="${imageRegalo}"></div>
         <div class="name-elem-reserva-lt data-elem-reserva-lt">${note.nombre}</div>
         <div class="dni-elem-reserva-lt data-elem-reserva-lt">${note.dni}</div>
         <div class="celular-elem-reserva-lt data-elem-reserva-lt">${note.celular}</div>
         <div class="lote-elem-reserva-lt data-elem-reserva-lt">${note.lote}</div>
-        <div class="regalo-elem-reserva-lt data-elem-reserva-lt">${note.regalo} </div>
         <div class="fecha-elem-reserva-lt data-elem-reserva-lt">${formattedDate} </div>
       </div>
       
