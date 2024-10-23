f236e9804befb58c2c0a45cdf14c3a05d6871b8c
KrishCM <44927837+KrishCM25@users.noreply.github.com>
2024-10-23 13:33:52 -0500

    9019 w98d09usdsd sdsdsdsdds

--- src/public/ui.js
+++ src/public/ui.js
@@ -47,8 +47,8 @@ const noteUI = (note) => {
       <div class="container-data-elem-reserva-lt">
         <div class="image-regalo-elem-reserva-lt" ${cssRegalo} ><img src="${imageRegalo}"></div>
         <div class="name-elem-reserva-lt data-elem-reserva-lt">${note.nombre}</div>
-        <div class="dni-elem-reserva-lt data-elem-reserva-lt">DNI ${note.dni}</div>
-        <div class="celular-elem-reserva-lt data-elem-reserva-lt">TEL ${note.celular}</div>
+        <div class="dni-elem-reserva-lt data-elem-reserva-lt"><span>DNI </span> ${note.dni}</div>
+        <div class="celular-elem-reserva-lt data-elem-reserva-lt"><span>TEL </span> ${note.celular}</div>
         <div class="lote-elem-reserva-lt data-elem-reserva-lt"><span>LOTE</span>${note.lote}
         <span class="tag-new-elem-reserva-lt ${isNewNoteText}">${isNewNoteText}</span>
         </div>
