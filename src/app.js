ef69b19f5f2056170e61c34fd7b98b303cb15196
KrishCM <44927837+KrishCM25@users.noreply.github.com>
2024-12-15 15:07:56 -0500

    hg asd

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import path from 'path';
 import axios from 'axios';
+import { config } from "dotenv";
 const fs = require('fs');
-
 const app = express();
 
+config();
 
 // Datos estáticos (puedes cambiar esto para recibir el DNI desde el cliente)
-const token = 'apis-token-11030.SCSv4kKYWlHpNtJT2xmm5h0Wd4NEHhOw';
+const tokenDNI = 'apis-token-11030.SCSv4kKYWlHpNtJT2xmm5h0Wd4NEHhOw';
 
 // Ruta para consultar el DNI
 app.get('/dni/:dni', async (req, res) => {
@@ -16,7 +17,7 @@ app.get('/dni/:dni', async (req, res) => {
     try {
         const response = await axios.get(`https://api.apis.net.pe/v2/reniec/dni?numero=${dni}`, {
             headers: {
-                'Authorization': `Bearer ${token}`,
+                'Authorization': `Bearer ${tokenDNI}`,
                 'Referer': 'https://apis.net.pe/consulta-dni-api'
             }
         });
@@ -65,34 +66,126 @@ app.get('/webhook', (req, res) => {
 
 // Ruta para recibir eventos
 app.post('/webhook', (req, res) => {
-    const body = req.body;
 
-    if (body.object === 'whatsapp_business_account') {
-        body.entry.forEach((entry) => {
-            const changes = entry.changes;
-            changes.forEach((change) => {
-                const messageData = change.value.messages;
-                if (messageData) {
-                    messageData.forEach((message) => {
-                        console.log('Mensaje recibido:', message);
-                        messages.push(message); // Guarda el mensaje en memoria
-                    });
-                }
-            });
-        });
-
-        // Guarda mensajes en un archivo JSON
-        fs.writeFileSync('messages.json', JSON.stringify(messages, null, 2));
-        res.sendStatus(200); // Confirma la recepción
-    } else {
-        res.sendStatus(404);
-    }
 });
 
 // Ruta para mostrar el archivo lotehg.html
 // app.get('/lotehg', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'lotehg.html'));
 // });
+
+
+// WEBHOOK WHATSAPP
+const token = process.env.WHATSAPP_TOKEN;
+// Accepts POST requests at /webhook endpoint
+app.post("/webhook", async (req, res) => {
+    // Parse the request body from the POST
+    let body = req.body;
+  
+    // Check the Incoming webhook message
+    console.log(JSON.stringify(req.body, null, 2));
+  
+    // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+    if (req.body.object) {
+      if (
+        req.body.entry &&
+        req.body.entry[0].changes &&
+        req.body.entry[0].changes[0] &&
+        req.body.entry[0].changes[0].value.messages &&
+        req.body.entry[0].changes[0].value.messages[0]
+      ) {
+        let phone_number_id =
+          req.body.entry[0].changes[0].value.metadata.phone_number_id;
+        let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+        let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+  
+        let openai_data = JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content: "Tu eres un asistente muy útil.",
+            },
+            {
+              role: "user",
+              content: msg_body,
+            },
+          ],
+        });
+  
+        let config = {
+          method: "post",
+          maxBodyLength: Infinity,
+          url: "https://api.openai.com/v1/chat/completions",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer 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",
+          },
+          data: openai_data,
+        };
+  
+        await axios
+          .request(config)
+          .then((response) => {
+            const response_data = response.data;
+            console.log(JSON.stringify(response.data));
+  
+            axios({
+              method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+              url:
+                "https://graph.facebook.com/v12.0/" +
+                phone_number_id +
+                "/messages?access_token=" +
+                token,
+              data: {
+                messaging_product: "whatsapp",
+                to: from,
+                text: { body: response_data.choices[0].message.content },
+              },
+              headers: { "Content-Type": "application/json" },
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+      res.sendStatus(200);
+    } else {
+      // Return a '404 Not Found' if event is not from a WhatsApp API
+      res.sendStatus(404);
+    }
+  });
+  
+  // Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+  // info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+  app.get("/webhook", (req, res) => {
+    /**
+     * UPDATE YOUR VERIFY TOKEN
+     *This will be the Verify Token value when you set up webhook
+     **/
+    const verify_token = process.env.VERIFY_TOKEN;
+  
+    // Parse params from the webhook verification request
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+  
+    // Check if a token and mode were sent
+    if (mode && token) {
+      // Check the mode and token sent are correct
+      if (mode === "subscribe" && token === verify_token) {
+        // Respond with 200 OK and challenge token from the request
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+      }
+    }
+  });
+
+
 
 // Middleware para servir archivos estáticos desde la carpeta 'public'
 app.use('/', express.static(path.join(__dirname, '/public')));
