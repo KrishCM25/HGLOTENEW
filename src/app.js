3fc5cc66addb649f3943ca8e735e8f6cf43948d7
KrishCM <44927837+KrishCM25@users.noreply.github.com>
2024-12-15 17:05:06 -0500

    hg asddsfasds098

--- src/app.js
+++ src/app.js
@@ -120,7 +120,7 @@ app.post("/webhook", async (req, res) => {
           headers: {
             "Content-Type": "application/json",
             Authorization:
-              `Bearer ${process.env.OPENAI_API_KEY_}`,
+              `Bearer ${process.env.OPENAI_API_KEY}`,
           },
           data: openai_data,
         };
