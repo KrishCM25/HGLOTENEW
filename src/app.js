51f15858f0bdef57f366c969a64b480d031895c3
KrishCM <44927837+KrishCM25@users.noreply.github.com>
2024-12-17 23:12:24 -0500

    hg asddsfasds098sdsadlkjdfsdsd

--- src/app.js
+++ src/app.js
@@ -58,52 +58,7 @@ app.get('/jamil', (req, res) => {
 const token = 'EAAhgbVzGu70BO7qUhhGYVZBrU2FibIU9HnkeIvOX3SyfNZBuGXDKJkCDKSKAIYvP1Ml5HrfMrBZBF3s9PAWzkGYcUIzAItyWx1zx2Lmw5rX0GKUeKbqtXmnZClXEKnd8Mh4ZAAYfJo70IhxG0MT2VMhZBPhGUuX1Xp1f5In2H22tXgPhNqJ5uY5eJHdUiBE6WMjgZDZD';
 // Accepts POST requests at /webhook endpoint
 app.post("/webhook", async (req, res) => {
-    // Parse the request body from the POST
-    let body = req.body;
-  
-    // Check the Incoming webhook message
-    console.log(JSON.stringify(req.body, null, 2));
-  
-    // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-    if (req.body) {
-      console.log('ENTRO ', req.body)
-      if (
-        req.body.entry &&
-        req.body.entry[0].changes &&
-        req.body.entry[0].changes[0] &&
-        req.body.entry[0].changes[0].value.messages &&
-        req.body.entry[0].changes[0].value.messages[0]
-      ) {
-        let phone_number_id =
-          req.body.entry[0].changes[0].value.metadata.phone_number_id;
-        let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-        let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-  
-        let openai_data = JSON.stringify({
-          model: "gpt-3.5-turbo",
-          messages: [
-            {
-              role: "system",
-              content: "Tu eres un asistente muy útil.",
-            },
-            {
-              role: "user",
-              content: msg_body,
-            },
-          ],
-        });
-  
-        let config = {
-          method: "post",
-          maxBodyLength: Infinity,
-          url: "https://api.openai.com/v1/chat/completions",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization:
-              'Bearer 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',
-          },
-          data: openai_data,
-        };
+
   
         await axios
           .request(config)
@@ -129,12 +84,7 @@ app.post("/webhook", async (req, res) => {
           .catch((error) => {
             console.log(error);
           });
-      }
-      res.sendStatus(200);
-    } else {
-      // Return a '404 Not Found' if event is not from a WhatsApp API
-      res.sendStatus(404);
-    }
+
   });
   
   // Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
